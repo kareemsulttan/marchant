1a6257554d723fa78a85b8b1437d8ea0dffaf035
Kareem Sultan <76824154+kareemsulttan@users.noreply.github.com>
2021-11-19 22:35:42 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ $('.btn-plus, .btn-minus').on('click', function(e) {
     input[0][isNegative ? 'stepDown' : 'stepUp']()
   }
 })
+$(".nav-item").click(function () {
+  // If the clicked element has the active class, remove the active class from EVERY .nav-link>.state element
+  if ($(this).hasClass("active")) {
+    $(".nav-item").removeClass("active");
+  }
+  // Else, the element doesn't have the active class, so we remove it from every element before applying it to the element that was clicked
+  else {
+    $(".nav-item").removeClass("active");
+    $(this).addClass("active");
+  }
+});
