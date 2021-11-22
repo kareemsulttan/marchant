123ea336b852c5a8e7458b9eee0820e205ba3bb3
Kareem Sultan <76824154+kareemsulttan@users.noreply.github.com>
2021-11-23 01:42:20 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -16,3 +16,30 @@ $(".nav-item").click(function () {
     $(this).addClass("active");
   }
 });
+/*modal*/
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+var close = document.getElementsByClassName("dismiss")[0];
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+  modal.style.display = "none";
+  console.log("works")
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdownBtn = document.querySelector(".dropdown-btn");
+
+dropdownBtn.onclick = () => {
+  dropdownMenu.classList.toggle("dropdown-active");
+};
+
